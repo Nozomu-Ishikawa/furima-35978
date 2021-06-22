41b1244d6f0e7f5b0a76cb9893217957289cf300
Nozomu-Ishikawa <koihime0musou@gmail.com>
2021-06-22 20:08:34 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   #itemsコントローラについてのルーティング
-  resources :items, only: [:index, :new, :create, :show, :edit]
+  resources :items, only: [:index, :new, :create, :show]
 
   #usersコントローラに対してのルーティング
 
